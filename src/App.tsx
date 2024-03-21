38cbb5888714890e373c09360866ab1e62b26cbc
Gustavo Lopez <gustavo@getkalto.com>
2024-03-21 15:53:04 -0300

    Custom hooks was added

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,14 @@ import Alert from './components/Alert';
 import Grid from './components/Grid';
 import Header from './components/Header';
 import useGrid from './hooks/useGrid';
-
-// const INIT_GRID = [
-//   ['', '', ''],
-//   ['', '', ''],
-//   ['', '', '']
-// ]
+import useWinner from './hooks/useWinner';
 
 function App() {
 
-  // const [grid, setGrid] = useState(INIT_GRID);
   const { grid, setGrid } = useGrid();
+  const { winner, validate } = useWinner();
 
   const [ turn, setTurn] = useState('X');
-  const [ winner, setWinner ] = useState<string|null>(null);
 
   const handleOnClick = (indexRow:number, indexCol:number) => {    
     setTurn( t => t === 'X' ? 'O' : 'X');
@@ -26,54 +20,7 @@ function App() {
     clonGrid[indexRow][indexCol] = turn;  
     setGrid(clonGrid);
 
-    validateWinner();
-
-  }
-
-  function validateWinner() {
-      const validationInline = () => {
-        let result = false;        
-
-        for (let i = 0; i < 3; i++) {
-          if ( !result && grid[i][0] !== '') {
-            result = grid[i][0] === grid[i][1] && grid[i][1] === grid[i][2]
-            result && setWinner(grid[i][0]);
-          }
-        }
-        
-        return result;
-      }
-
-      const validateBlock = () => {
-        let result = false; 
-        
-        for (let i = 0; i < 3; i++) {
-          if ( !result && grid[0][i] !== '') {
-            result = grid[0][i] === grid[1][i] && grid[1][i] === grid[2][i]             
-            result && setWinner(grid[0][i]);
-          }
-        }
-
-        return result;
-      }
-
-      const validateDiagonal = () => {
-        let result = false;
-
-        if ( grid[1][1] !== '') {
-          const val = grid[1][1];
-
-          result = val === grid[0][0] && val === grid[2][2] ||
-                   val === grid[2][0] && val === grid[0][2]
-
-          result && setWinner(val);            
-        }
-
-        return result;
-      }
-
-      return validationInline() || validateBlock() || validateDiagonal();
-    
+    validate.isWinner(clonGrid);
   }
 
   return (
---
+++ src/hooks/useWinner.ts
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+const useWinner = () => {
+	const [winner, setWinner] = useState<string | null>(null);
+
+	function validateWinner(grid: string[][]) {
+		const validationInline = () => {
+			let result = false;
+
+			for (let i = 0; i < 3; i++) {
+				if (!result && grid[i][0] !== "") {
+					result =
+						grid[i][0] === grid[i][1] && grid[i][1] === grid[i][2];
+					result && setWinner(grid[i][0]);
+				}
+			}
+
+			return result;
+		};
+
+		const validateBlock = () => {
+			let result = false;
+
+			for (let i = 0; i < 3; i++) {
+				if (!result && grid[0][i] !== "") {
+					result =
+						grid[0][i] === grid[1][i] && grid[1][i] === grid[2][i];
+					result && setWinner(grid[0][i]);
+				}
+			}
+
+			return result;
+		};
+
+		const validateDiagonal = () => {
+			let result = false;
+
+			if (grid[1][1] !== "") {
+				const val = grid[1][1];
+
+				result =
+					(val === grid[0][0] && val === grid[2][2]) ||
+					(val === grid[2][0] && val === grid[0][2]);
+
+				result && setWinner(val);
+			}
+
+			return result;
+		};
+
+		return validationInline() || validateBlock() || validateDiagonal();
+	}
+
+	return {
+		winner,
+		validate: {
+			isWinner: validateWinner,
+		},
+	};
+};
+
+export default useWinner;
